73d4ab58e246b16eed4b80e8fb055311e829ffee
rzjala <jalanr@code.edu.az>
2021-08-18 10:21:07 +0400

    task-7

---
+++ 08-18/08-18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Salam";
+            Console.WriteLine(reverse(word));
+        }
+        static string reverse(string word)
+        {
+
+            string reversed = "";
+            int length;
+
+            length = word.Length - 1;
+
+            while (length >= 0)
+            {
+                reversed = reversed + word[length];
+                length--;
+            }
+            return reversed;
+        }
+        
+     
+  
+}  
+    }
+
